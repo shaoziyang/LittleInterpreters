e80dbb502ac2b201f180bb129dac7596c192ef35
shaoziyang <shaoziyang@126.com>
2020-04-02 18:05:14 +0800

    update NewFileTemplate

--- NewFileTemplate.pas
+++ NewFileTemplate.pas
@@ -10,8 +10,18 @@ const
   NewPasFileTemplate: string = (
   'program newfile;' + #13#10 +
   'var' + #13#10 +
-  '  i: integer;' + #13#10 +
+  '  i: integer;' + #13#10#13#10 +
+  '  procedure f1;' + #13#10 +
+  '  begin' + #13#10#13#10 +
+  '  end;' + #13#10#13#10 +
   'begin' + #13#10 +
-  '  print([1, 1+2*3.4/5, ''Hello''])' + #13#10 +
+  '  print([1, 1+2 * 3.4 / 5, ''Hello''])' + #13#10 +
   'end.' 
+    );
+
+  NewBasFileTemplate: string = (
+  '10 FOR i = 1 TO 10' + #13#10 +
+  '20 PRINT i;' + #13#10 +
+  '30 NEXT i' + #13#10 +
+  '40 PRINT'
     );
